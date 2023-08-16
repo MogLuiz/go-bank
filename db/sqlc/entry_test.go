bfd8559f57e04812bb8c637cf624dd2e982688d0
Luiz Henrique <luizhjramos@outlook.com>
2023-08-16 07:35:28 -0300

    test(unit): ensure be able to create entry

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/MogLuiz/go-bank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	args := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    utils.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, args.AccountID, entry.AccountID)
+	require.Equal(t, args.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
